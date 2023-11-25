e5df98c4b7b5d480e15f20233f9cf93334dbe50d
ZBegod <zohidovbegzod4557@gmail.com>
2023-11-25 21:10:12 +0500

    register models

--- Article/admin.py
+++ Article/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from Article.models import (
+    Article, ArticleImages, Category
+)
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Article)
+admin.site.register(ArticleImages)
