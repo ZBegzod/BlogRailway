957cbc1eeec72d405666b2c97833981015abbdaa
ZBegod <zohidovbegzod4557@gmail.com>
2023-11-26 09:12:17 +0500

    add category crud view

--- Article/views.py
+++ Article/views.py
@@ -4,14 +4,61 @@ from rest_framework import status
 from .pagination import Pagination
 from rest_framework import generics
 
-from Article.models import Article
+from Article.models import Article, Category
 from rest_framework.response import Response
+from rest_framework.mixins import (
+    ListModelMixin, CreateModelMixin,
+    RetrieveModelMixin, DestroyModelMixin
+)
 from Article.serializer import (
-    ArticleModelSerializer, DestroyModelSerializer
+    ArticleModelSerializer,
+    DestroyModelSerializer,
+    CategoryModelSerializer
 )
 
 
 # Create your views here.
+class CategoryDetailAPIView(RetrieveModelMixin,
+                            DestroyModelMixin,
+                            generics.GenericAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategoryModelSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CreateCategory(ListModelMixin, CreateModelMixin,
+                     generics.GenericAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategoryModelSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        context = {'request': request}
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            serializer = self.serializer_class(page, many=True, context=context)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.serializer_class(queryset, many=True, context=context)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ArticleListAPIView(generics.ListAPIView):
     queryset = Article.objects.all()
